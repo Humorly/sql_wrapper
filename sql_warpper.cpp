038ddda7b414eb19dc27e3b62449bf09cd9b2cc3
陈安卓 <chenanzhuo517@gmail.com>
2020-04-10 22:47:31 +0800

    移除接口调用模板参数

--- sql_warpper.cpp
+++ sql_warpper.cpp
@@ -1,4 +1,4 @@
-﻿
+
 #include <cstdlib>
 #include <iostream>
 #include "sql_warpper.h"
@@ -10,18 +10,17 @@ int main(void)
 
 	sql_warpper sql_("tcp://127.0.0.1:3306", "root", "407043", "game");
 	bool ret_ = false;
-	ret_ = sql_.create("create table user_information (id_ bigint primary key, name_ varchar(64), password_ varchar(64)); ");
-	ret_ = sql_.insert("insert into user_information values(512056683, \"humorly\", \"chen123\");");
-	ret_ = sql_.insert("insert into user_information values(512056684, \"humorly\", \"chen123\");");
-	ret_ = sql_.insert("insert into user_information values(512056685, \"humorly\", \"chen123\");");
-	ret_ = sql_.remove("delete from user_information  where id_ = 512056684;");
-	ret_ = sql_.update("update game.user_information set id_ = 555555555 where id_ = 512056683;");
+	ret_ = sql_.create("create table test (id_ double, name_ varchar(64), password_ varchar(64)); ");
+	ret_ = sql_.insert("insert into test values(5.12056683, \"humorly\", \"chen123\");");
+	ret_ = sql_.insert("insert into test values(5.12056684, \"humorly\", \"chen123\");");
+	ret_ = sql_.insert("insert into test values(5.12056685, \"humorly\", \"chen123\");");
+	ret_ = sql_.remove("delete from test  where id_ = 5.12056684;");
+	ret_ = sql_.update("update game.test set id_ = 5.55555555 where id_ = 5.12056683;");
 
 	// 待搜索的字段信息
 	std::tuple<std::string, std::string, std::string> tuple_("id_", "name_", "password_");
-	std::vector<std::tuple<integer_type, std::string, std::string>> user_content_;
-	ret_ = sql_.select<std::tuple<std::string, std::string, std::string>,
-		integer_type, std::string, std::string>("SELECT * FROM user_information", user_content_, tuple_);
+	std::vector<std::tuple<char, std::string, std::string>> user_content_;
+	ret_ = sql_.select("SELECT * FROM test", user_content_, tuple_);
 
 	std::cout << "content:" << std::endl;
 	for (auto & val : user_content_)
@@ -33,3 +32,5 @@ int main(void)
 
 	return EXIT_SUCCESS;
 }
+
+
